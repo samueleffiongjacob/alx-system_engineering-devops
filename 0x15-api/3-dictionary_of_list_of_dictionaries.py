3c051f237f43c16a6b77580c1108d5e2cec7e3f7
samueleffiongjacob <samueleffiong685@gmail.com>
2023-04-24 21:23:55 +0100

    first Api work for Alx and some introdustion topic to  REST API PEPD8 PYTHON STYLE

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """Exports to-do list information of all employees to JSON format."""
 import json
-import requests
+import requests as sam
 
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
-    users = requests.get(url + "users").json()
+    users = sam.get(url + "users").json()
 
     with open("todo_all_employees.json", "w") as jsonfile:
         json.dump({
@@ -13,6 +13,6 @@ if __name__ == "__main__":
                 "task": t.get("title"),
                 "completed": t.get("completed"),
                 "username": u.get("username")
-            } for t in requests.get(url + "todos",
+            } for t in sam.get(url + "todos",
                                     params={"userId": u.get("id")}).json()]
             for u in users}, jsonfile)
