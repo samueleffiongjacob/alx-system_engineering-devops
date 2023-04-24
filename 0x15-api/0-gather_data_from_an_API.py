08e3d0f1715d6fff069b5cee3139b7e18affee19
samueleffiongjacob <samueleffiong685@gmail.com>
2023-04-24 22:58:33 +0100

    first Api work for Alx and some introdustion topic to  REST API PEPD8 PYTHON STYLE

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,15 +7,11 @@ import sys
 REST_API = "https://jsonplaceholder.typicode.com"
 
 if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        id = int(sys.argv[1])
-        employee = sam.get('{}/users/{}'.format(REST_API, id)).json()
-        task_req = sam.get('{}/todos'.format(REST_API)).json()
-        employee_name = employee.get('name')
-        tasks = list(filter(lambda x: x.get('userId') == id, task_req))
-        completed_tasks = list(filter(lambda x: x.get('completed'), tasks))
-        print('Employee {} is done with tasks({}/{}):'.format(employee_name,
-                len(completed_tasks), len(tasks)))
-        if len(completed_tasks) > 0:
-            for task in completed_tasks:
-                print('\t{}'.format(task.get('title')))
+       
+    employee_name = sam.get(REST_API + "users/{}".format(sys.argv[1])).json()
+    tasks = sam.get(REST_API + "todos", params={"userId":sys.argv[1]}).json()
+
+    completed_tasks = [t.get("title") for t in todos if t.get("completed") is True]
+    print("Employee {} is done with tasks({}/{}):".format(employee_name.get('name'),
+        len(completed_tasks), len(tasks)))
+    [print("\t {}".format(c)) for c in completed_tasks]
