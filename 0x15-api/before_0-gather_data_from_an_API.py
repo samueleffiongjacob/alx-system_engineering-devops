ff747e7c01c749a0e6c765012de1b809b966543f
samueleffiongjacob <samueleffiong685@gmail.com>
2023-04-24 22:20:08 +0100

    first Api work for Alx and some introdustion topic to  REST API PEPD8 PYTHON STYLE

--- 0x15-api/before_0-gather_data_from_an_API.py
+++ 0x15-api/before_0-gather_data_from_an_API.py
@@ -4,6 +4,17 @@ returns information about his/her TODO list progress."""
 import requests as sam
 import sys
 
+"""if __name__ == '__main__':
+    url = 'https://jsonplaceholder.typicode.com/'
+    usr_id = sam.get(url + 'users/{}'.format(sys.argv[1])).json()
+    to_do = sam.get(url + 'todos', params={'userId': sys.argv[1]}).json()
+#    print(to_do)
+    completed = [title.get("title") for title in to_do if title.get('completed') is True]
+    # print(completed)
+    print("Employee {} is done with tasks({}/{}):".format(usr_id.get("name"),len(completed),len(to_do)))
+    [print("\t {}".format(title)) for title in completed]"""
+
+
 REST_API = "https://jsonplaceholder.typicode.com"
 
 if __name__ == '__main__':
