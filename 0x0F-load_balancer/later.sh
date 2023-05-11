941023ec63f9ecca65bca21f733b90c0a126b86f
samueleffiongjacob <samueleffiong685@gmail.com>
2023-05-11 05:40:41 +0100

    HTTPS FINALL FIXED

---
+++ 0x0F-load_balancer/later.sh
@@ -0,0 +1,15 @@
+frontend effiongsamuel-frontend-https
+    bind *:80
+    bind *:443 ssl crt /etc/haproxy/certs/effiongsamuel.tech.pem
+    http-request redirect scheme https code 301 unless { ssl_fc }
+    http-request set-header X-Forwarded-Proto https
+    default_backend effiongsamuel-backend
+
+
+errorfile 400 /etc/haproxy/errors/400.http
+    errorfile 403 /etc/haproxy/errors/403.http
+    errorfile 408 /etc/haproxy/errors/408.http
+    errorfile 500 /etc/haproxy/errors/500.http
+    errorfile 502 /etc/haproxy/errors/502.http
+    errorfile 503 /etc/haproxy/errors/503.http
+    errorfile 504 /etc/haproxy/errors/504.http
