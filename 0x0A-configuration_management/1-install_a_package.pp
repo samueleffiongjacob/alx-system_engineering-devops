4a95471825ca735d50cf2f6fffe9580aa0d645e5
samueleffiongjacob <samueleffiongjacob@gmail.com>
2023-12-13 20:48:10 +0100

    You are expected to delete the following repositories and folders from your GitHub account. Failure to do so will have a negative impact on your experience and score in your future cohor

--- 0x0A-configuration_management/1-install_a_package.pp
+++
@@ -1,9 +0,0 @@
-# A puppet manifest installing flask v2.1.0 which is a package from pip3.
-# Am also specifying the version of flask to install using the ensure attribute
-# flask -- version to check version of python and flask install
-# check the read me file under resource to see what ensure, provider means in puppet
-
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => pip3,
-}
--- 0x12-web_stack_debugging_2/README.md
+++
@@ -1,2 +0,0 @@
-# 0x12. Web stack debugging #2 
-
