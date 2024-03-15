56e09fa490ba0467bfef042b71c0ae4090656782
SAMUEL EFFIONG <101840641+samueleffiongjacob@users.noreply.github.com>
2024-03-15 03:26:52 +0000

    new

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -4,3 +4,4 @@ puts ARGV[0].scan(/School/).join
 # The regular expression must match School
 # Using the project instructions, create a Ruby script that accepts one argument and pass it to a regular expression matching method
 
+# ./0-simply_match_school.rb School | cat -e
