06f6906825911b7ac1747cf7eca7fabf5ce2acd3
samueleffiongjacob <samuelefffiongjacob@gmail.com>
2024-08-25 21:43:39 +0100

    ctr

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,30 +1,3 @@
-#!/usr/bin/python3
-"""Function to query subscribers on a given Reddit subreddit."""
-import requests
-
-
-def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
-        return 0
-    elif response.status_code != 200:
-        # Handle other potential HTTP errors (e.g., 500, 403, etc.)
-        return 0
-    
-    try:
-        data = response.json().get("data", {})
-        return data.get("subscribers", 0)
-    except ValueError:
-        # Handle JSON decoding errors
-        return 0
-
-
-
 # #!/usr/bin/python3
 # """Function to query subscribers on a given Reddit subreddit."""
 # import requests
@@ -39,5 +12,32 @@ def number_of_subscribers(subreddit):
 #     response = requests.get(url, headers=headers, allow_redirects=False)
 #     if response.status_code == 404:
 #         return 0
-#     results = response.json().get("data")
-#     return results.get("subscribers")
+#     elif response.status_code != 200:
+#         # Handle other potential HTTP errors (e.g., 500, 403, etc.)
+#         return 0
+    
+#     try:
+#         data = response.json().get("data", {})
+#         return data.get("subscribers", 0)
+#     except ValueError:
+#         # Handle JSON decoding errors
+#         return 0
+
+
+
+#!/usr/bin/python3
+"""Function to query subscribers on a given Reddit subreddit."""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
+        return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
